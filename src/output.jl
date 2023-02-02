70fa0341bdd0dd6019a4f9bfc57ca29b18de7cc4
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-02-02 09:33:34 -0500

    get_hf_energy

--- src/output.jl
+++ src/output.jl
@@ -21,3 +21,11 @@ end
 function Base.show(io::IO, out::OutputFile)
     println(io, collect(keys(out.contents)))
 end
+
+const hf_energy_regex::Regex =
+    r"Total energy: +(-?\d+\.\d+)"
+
+export get_hf_energy
+function get_hf_energy(out::OutputFile)
+    parse(Float64, match(hf_energy_regex, out.contents["eT"]).captures[1])
+end
