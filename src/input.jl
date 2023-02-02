8fa77c56ccb1a0aa85c9870e36327a5d5443979e
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-02-02 08:22:06 -0500

    basic input file structure

--- src/input.jl
+++ src/input.jl
@@ -1,9 +1,18 @@
+export InputFile, make_input_hf
 
 struct InputFile
     molecule::Molecule{Atom}
     basis::String
 
-    methods::Vector{String}
+    sections::Dict{String,Vector{Any}}
+end
+
+function InputFile(molecule, basis)
+    InputFile(
+        molecule,
+        basis,
+        Dict{String,Vector{Any}}()
+    )
 end
 
 function Base.show(io::IO, inp::InputFile)
@@ -12,5 +21,45 @@ function Base.show(io::IO, inp::InputFile)
     if inp.molecule.multiplicity != 1
         println(io, "    multiplicity: ", inp.molecule.multiplicity)
     end
-    println(io, "end system")
+    println(io, "end system\n")
+
+    for (section, fields) in inp.sections
+        println(io, section)
+        for field in fields
+            if field isa Pair
+                fieldname, value = field
+                println(io, "    ", fieldname, ": ", value)
+            else
+                println(io, "    ", field)
+            end
+        end
+        println(io, "end ", section, "\n")
+    end
+
+    println(io, "geometry")
+    println(io, "basis: ", inp.basis)
+    print(io, Molecules.get_xyz(inp.molecule))
+    println(io, "end geometry")
+end
+
+function add_fields!(inp::InputFile, args...)
+    for (section, fields) in args
+        if haskey(inp.sections, section)
+            append!(inp.sections[section], fields)
+        else
+            inp.sections[section] = fields
+        end
+    end
+end
+
+function make_input_hf(molecule, basis, args...)
+    inp = InputFile(molecule, basis)
+
+    inp.sections["do"] = ["ground state"]
+    inp.sections["memory"] = ["available" => 8]
+    inp.sections["method"] = ["hf"]
+
+    add_fields!(inp, args...)
+
+    inp
 end
