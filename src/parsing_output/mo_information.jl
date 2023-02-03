27b5788b2995d575f5eb87ad301673feb42097c5
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-02-03 04:44:26 -0500

    mo_energy

--- src/parsing_output/mo_information.jl
+++ src/parsing_output/mo_information.jl
@@ -3,4 +3,29 @@ function write_molden(io::IO, out::OutputFile)
     write(io, out.contents["eT.molden"])
 end
 
-# TODO: parse mo_information file
+const mo_energy_regex1 = r"- Molecular orbital energies\s+-+\n(.+?)\n  -+"s
+const mo_energy_regex2 = r"(\d+) +(-?\d+\.\d+)"
+
+export get_mo_energy
+function get_mo_energy(out::OutputFile)
+    d = Dict{Int,Float64}()
+
+    mo_energy_string = match(
+        mo_energy_regex1, out.contents["eT.mo_information"]
+    ).captures[1]
+
+    for m in eachmatch(mo_energy_regex2, mo_energy_string)
+        n = parse(Int, m.captures[1])
+        e = parse(Float64, m.captures[2])
+
+        d[n] = e
+    end
+
+    energies = zeros(length(d))
+
+    for (n, e) in d
+        energies[n] = e
+    end
+
+    energies
+end
