f9576c03cac762c5123b3a5099880ec8d045049a
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-02-08 04:16:10 -0500

    libcint release mode

--- deps/build-libcint.sh
+++ deps/build-libcint.sh
@@ -4,5 +4,5 @@ cd $1
 mkdir build
 cd build
 
-cmake .. -DBUILD_SHARED_LIBS=0 -DPYPZPX=1 -DCMAKE_INSTALL_PREFIX=../install -GNinja -DCMAKE_C_COMPILER=$2/gcc
+cmake .. -DBUILD_SHARED_LIBS=0 -DPYPZPX=1 -DCMAKE_INSTALL_PREFIX=../install -GNinja -DCMAKE_C_COMPILER=$2/gcc -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_MARCH_NATIVE=1
 cmake --build . --target install
